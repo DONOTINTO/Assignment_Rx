881d7bc2aeec6ff23549b5fd3496524cda3f6ea2
DONOTINTO <goods99j@gmail.com>
2024-03-29 19:30:52 +0900

    update. 프로젝트 추가 및 0328 3번

--- SeSACRxThreads/SignIn/SignInViewController.swift
+++ SeSACRxThreads/SignIn/SignInViewController.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 import SnapKit
+import RxSwift
+import RxCocoa
 
 class SignInViewController: UIViewController {
 
@@ -15,6 +17,8 @@ class SignInViewController: UIViewController {
     let signInButton = PointButton(title: "로그인")
     let signUpButton = UIButton()
     
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,14 +26,30 @@ class SignInViewController: UIViewController {
         
         configureLayout()
         configure()
+        bind()
+    }
+    
+    func bind() {
         
-        signUpButton.addTarget(self, action: #selector(signUpButtonClicked), for: .touchUpInside)
+        signUpButton.rx.tap.bind(with: self) { owner, _ in
+            owner.navigationController?.pushViewController(SignUpViewController(), animated: true)
+        }.disposed(by: disposeBag)
+        
+        signInButton.rx.tap.bind(with: self) { owner, _ in
+            print("회원가입 완료")
+        }.disposed(by: disposeBag)
+        
+        let emailValid = emailTextField.rx.text.orEmpty.map { $0.count >= 5}
+        let passwordValid = passwordTextField.rx.text.orEmpty.map { $0.count >= 5 }
+        let everythingValid = Observable.combineLatest(emailValid, passwordValid).map { $0 && $1 }
+        
+        emailValid.bind(to: passwordTextField.rx.isEnabled).disposed(by: disposeBag)
+        everythingValid.bind(to: signInButton.rx.isEnabled).disposed(by: disposeBag)
+        
     }
-    
-    @objc func signUpButtonClicked() {
-        navigationController?.pushViewController(SignUpViewController(), animated: true)
-    }
-    
+}
+
+extension SignInViewController {
     
     func configure() {
         signUpButton.setTitle("회원이 아니십니까?", for: .normal)
@@ -66,6 +86,4 @@ class SignInViewController: UIViewController {
             make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
         }
     }
-    
-
 }
